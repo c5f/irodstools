dc66098f907e350a880ded8311c1a747c27a4334
Colin Scott-Fleming <colinsf@arizona.edu>
2012-08-28 13:07:41 -0700

    Initial copy of files from Drupal environment.

---
+++ irodstools/irods_directory/irods_directory.js
@@ -0,0 +1,8 @@
+// $Id$
+
+// jQuery(document).ready(function() {
+//   input = jQuery('#field-irods-directory-values input');
+//   jQuery(input).focus(function() {
+//     this.attr('value', pick());
+//   });
+// });
